28779e0606e75e79ac57259ecaa019efb70d4a06
fuzzybear3965 <johnrichardrinehart@gmail.com>
2017-01-06 15:43:51 -0500

    added initial conditions

--- src/generateAllPathsOfLengthN.jl
+++ src/generateAllPathsOfLengthN.jl
@@ -1,3 +1,11 @@
 function generateAllPathsOfLengthN(n::Int)
- return filter(path -> !any(cumsum(path).<0), multiset_permutations(vcat(ones(Int8,n),-ones(Int8,n)),2*n))
+   depth = n - 1
+   if depth == 0
+      return [[Int8(1), Int8(-1)]]
+   end
+   paths = collect(filter(path -> !any(cumsum(path).<0),
+                          multiset_permutations(vcat(ones(Int8,depth),-ones(Int8,depth)),2*depth)))
+   unshift!.(paths,Int8(1))
+   push!.(paths,Int8(-1))
+   return paths
 end
