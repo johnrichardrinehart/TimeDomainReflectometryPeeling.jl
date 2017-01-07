8d603ad6d505e93df94f581f88a812a583e3fb76
fuzzybear3965 <johnrichardrinehart@gmail.com>
2017-01-06 19:50:39 -0500

    completed a working version of reflectedVoltageSignalToReflectionCoefficients

--- src/TDRPeeling.jl
+++ src/TDRPeeling.jl
@@ -7,11 +7,13 @@ include("./transitionFromPath.jl")
 include("./relativeReflectionCoefficientsToAbsoluteImpedances.jl")
 include("./generateAllPathsOfLengthN.jl")
 include("./pathWeightCalculator.jl")
+include("./reflectedVoltageSignalToReflectionCoefficients.jl")
 
 export reflectionCoefficientsToReflectedVoltageSignal
 export transitionFromPath
 export relativeReflectionCoefficientsToAbsoluteImpedances
 export generateAllPathsOfLengthN
 export pathWeightCalculator
+export reflectedVoltageSignalToReflectionCoefficients
 
 end
--- src/reflectionCoefficientsToReflectedVoltageSignal.jl
+++ src/reflectionCoefficientsToReflectedVoltageSignal.jl
@@ -1,14 +1,3 @@
 function reflectionCoefficientsToReflectedVoltageSignal(ref_coeffs, time_steps)
-paths_amplitude = zeros(length(time_steps),1)
-#for i = 1:time_steps
-for i = 5
-  # the below generates all valid paths of length.
-  # note that these paths lack the initial 1 (entering) and the last -1 (exiting)
-  paths = filter(path -> !any(cumsum(path).<0),multiset_permutations(vcat(ones(Int,i),-ones(Int,i)),2*i))
-  cur_transitions = transitionsFromPaths(collect(paths))
-  return cur_transitions
-#  depths = max(ref_coeffs,2);
-  # time_steps may be greater than number of reflection planes
-  #filter!(x -> not(max(x) > size(ref_coeffs,1)),cur_transitions); # update cur_transitions
-end
+
 end
