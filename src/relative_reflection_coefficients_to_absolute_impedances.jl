bc87acc71b44be83976b0e73e054d41b9ef95e3f
fuzzybear3965 <johnrichardrinehart@gmail.com>
2017-01-31 21:13:29 -0500

    fixed dimensions of impedances

--- src/relative_reflection_coefficients_to_absolute_impedances.jl
+++ src/relative_reflection_coefficients_to_absolute_impedances.jl
@@ -1,5 +1,5 @@
 function relative_reflection_coefficients_to_absolute_impedances(ref_coeffs, ref_impedance)
-   impedances = zeros(length(ref_coeffs),1)
+   impedances = zeros(length(ref_coeffs))
    impedances[1] = ref_impedance * (1+ref_coeffs[1])/(1-ref_coeffs[1])
    for i = 2:length(ref_coeffs)
       impedances[i] = impedances[i-1] * (1+ref_coeffs[i])/(1-ref_coeffs[i])
