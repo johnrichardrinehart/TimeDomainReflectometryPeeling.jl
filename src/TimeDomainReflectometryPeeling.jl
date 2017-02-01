da5f0ab873f4c0fec45f59de63fd0e9293294a93
fuzzybear3965 <johnrichardrinehart@gmail.com>
2017-01-31 21:13:02 -0500

    added chinese peeling

--- src/TimeDomainReflectometryPeeling.jl
+++ src/TimeDomainReflectometryPeeling.jl
@@ -8,9 +8,12 @@ include("./generate_all_paths_of_length_n.jl")
 include("./path_weight_calculator.jl")
 include("./reflected_voltage_signal_to_reflection_coefficients.jl")
 include("./count_sub_vecs.jl")
+include("./triangular_array.jl")
+include("./chinese_peeling.jl")
 
 export reflection_coefficients_to_reflected_voltage_signal
 export relative_reflection_coefficients_to_absolute_impedances
 export reflected_voltage_signal_to_reflection_coefficients
+export TriangularArray
 
 end
